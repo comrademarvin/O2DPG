792da2477f1f059ffbe9f1dd9aa889f0b05609a1
comrademarvin <stephan.stefie@gmail.com>
2025-06-24 22:03:11 +0000

    test without trigger

--- MC/config/MyTasks/scripts/fwd_recon.sh
+++ MC/config/MyTasks/scripts/fwd_recon.sh
@@ -1,4 +1,4 @@
-o2-sim-digitizer-workflow
+o2-sim-digitizer-workflow --configKeyValues "HBFUtils.nHBFPerTF=32;HBFUtils.orbitFirst=29098560;HBFUtils.orbitFirstSampled=36559744"
 o2-mft-reco-workflow
 o2-mid-digits-reader-workflow | o2-mid-reco-workflow | o2-mch-reco-workflow | o2-muon-tracks-matcher-workflow --disable-root-input | o2-dpl-run --run --no-batch
 o2-globalfwd-matcher-workflow --configKeyValues "FwdMatching.useMIDMatch=true"
