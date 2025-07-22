4664efec044610df2c07ab7aea0490705ad7431c
comrademarvin <stephan.stefie@gmail.com>
2025-07-22 12:06:33 +0000

    lower pT cut for W

--- MC/run/MyTasks/run_W_mu_fwd_pythia.sh
+++ MC/run/MyTasks/run_W_mu_fwd_pythia.sh
@@ -14,9 +14,9 @@ NSIGEVENTS=100
 NWORKERS=20
 NTIMEFRAMES=1
 
-${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 5360 -seed 12345 -col pp -gen pythia8 -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -e TGeant4 \
+${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 13600 -seed 12345 -col pp -gen pythia8 -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -e TGeant4 \
     -interactionRate 500000 -run 559361 --readoutDets MFT,MCH,MID,ITS \
-	-confKey "GeneratorPythia8.config=/home/stephan/alice/O2DPG/MC/config/MyTasks/pythia8/pythia_W_mu.cfg;GeneratorPythia8.includePartonEvent=true"
-    #-trigger "external" -ini /home/stephan/alice/O2DPG/MC/config/MyTasks/ini/trigger_W_mu_pythia.ini \
+	-confKey "GeneratorPythia8.config=/home/stephan/alice/O2DPG/MC/config/MyTasks/pythia8/pythia_W_mu.cfg;GeneratorPythia8.includePartonEvent=true" \
+    -trigger "external" -ini /home/stephan/alice/O2DPG/MC/config/MyTasks/ini/trigger_W_mu_pythia.ini
 
 ${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt aod --cpu-limit 20 --mem-limit 40000
