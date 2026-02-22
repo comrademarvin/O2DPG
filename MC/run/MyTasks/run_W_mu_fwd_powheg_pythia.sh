887ace7892280953720d61de1494595179b0affd
comrademarvin <stephan.stefie@gmail.com>
2026-02-22 10:01:15 +0000

    adding task for running powheg in o2sim

---
+++ MC/run/MyTasks/run_W_mu_fwd_powheg_pythia.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# make sure O2DPG + O2 is loaded
+[ ! "${O2DPG_ROOT}" ] && echo "Error: This needs O2DPG loaded" && exit 1
+[ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
+
+# ----------- SETUP LOCAL CCDB CACHE --------------------------
+export ALICEO2_CCDB_LOCALCACHE=$PWD/.ccdb
+
+# ----------- LOAD UTILITY FUNCTIONS --------------------------
+. ${O2_ROOT}/share/scripts/jobutils.sh
+
+# number of timeframes to simulate
+NTFS=${NTFS:-1}
+# number of simulation workers per timeframe
+NWORKERS=${NWORKERS:-20}
+# number of events to be simulated per timeframe
+NEVENTS=${NEVENTS:-100}
+
+${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 13600 -seed 12345 -col pp -gen external -j ${NWORKERS} -ns ${NEVENTS} -tf ${NTFS} -e TGeant4 \
+    -interactionRate 691338 -run 539874 -mod "--skipModules ZDC" \
+    -trigger "external" -ini /home/stephan/alice/O2DPG/MC/config/MyTasks/ini/trigger_W_mu_powheg.ini \
+	-confKey "GeneratorPythia8.config=/home/stephan/alice/O2DPG/MC/config/MyTasks/pythia8/pythia8_powheg.cfg;GeneratorPythia8.includePartonEvent=true"
+
+${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt aod --cpu-limit 20 --mem-limit 40000
