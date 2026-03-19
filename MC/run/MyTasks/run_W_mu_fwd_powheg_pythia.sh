2382beaf44518403e9b2412c04c58222d0166d28
comrademarvin <stephan.stefie@gmail.com>
2026-03-19 15:39:11 +0000

    adding W task for powheg-pythia in O2sim

--- MC/run/MyTasks/run_W_mu_fwd_powheg_pythia.sh
+++ MC/run/MyTasks/run_W_mu_fwd_powheg_pythia.sh
@@ -15,11 +15,11 @@ NTFS=${NTFS:-1}
 # number of simulation workers per timeframe
 NWORKERS=${NWORKERS:-20}
 # number of events to be simulated per timeframe
-NEVENTS=${NEVENTS:-100}
+NEVENTS=${NEVENTS:-1000}
 
 ${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 13600 -seed 12345 -col pp -gen external -j ${NWORKERS} -ns ${NEVENTS} -tf ${NTFS} -e TGeant4 \
     -interactionRate 691338 -run 539874 -mod "--skipModules ZDC" \
-    -trigger "external" -ini /home/stephan/alice/O2DPG/MC/config/MyTasks/ini/trigger_W_mu_powheg.ini \
+    -trigger "external" -ini /home/stephan/alice/O2DPG/MC/config/MyTasks/ini/GeneratorPythia8Powheg_W_mu_fwd.ini \
 	-confKey "GeneratorPythia8.config=/home/stephan/alice/O2DPG/MC/config/MyTasks/pythia8/pythia8_powheg.cfg;GeneratorPythia8.includePartonEvent=true"
 
 ${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt aod --cpu-limit 20 --mem-limit 40000
